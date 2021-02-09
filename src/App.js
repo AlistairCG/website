e648be16cf74c0c222f7624781cddd3c8fffbb0d
Alistair <agodwin@myseneca.ca>
2021-02-09 18:17:09 -0500

    Add gh deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <div className="App">
       <NavBar></NavBar>
         <header className="App-header">
-    
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
